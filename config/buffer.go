50abec2494c1adb23f97ac883a63adfaaca99247
Joshua Moses <59664518+joshuanmoses@users.noreply.github.com>
2026-08-25 12:47:40 -0500

    Delete config directory

--- config/buffer.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import "bytes"
-
-// Small aliases keep the YAML plumbing in load.go readable without importing bytes
-// throughout the package.
-type writeBuffer = bytes.Buffer
-
-func bytesReader(b []byte) *bytes.Reader { return bytes.NewReader(b) }
